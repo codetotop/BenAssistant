62a8e806f4891136e9fd0744f1ec051a36e0ed14
DungNB <dungnb@nal.vn>
2025-12-30 15:44:51 +0700

    Android lint

--- app/src/main/java/com/example/benassistant/MainContract.kt
+++ app/src/main/java/com/example/benassistant/MainContract.kt
@@ -5,17 +5,13 @@ import com.example.benassistant.room.ChatLog
 interface MainContract {
 
     interface View {
-
         fun loadMessages(messages: List<ChatLog>? = listOf())
         fun addMessage(message: ChatLog)
         fun setMessage(message: ChatLog)
         fun removeLastMessage()
-
         fun beginTransactionUI()
         fun closeTransactionUI()
-
         fun clearInput()
-
         fun scrollToBottom()
         fun showError(message: String)
     }
